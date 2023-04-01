2d9967a8010139ac1487e51ff2ddefdb7a05770d
shawnbradyrobinson <shawnbradyrobinson1@gmail.com>
2023-04-01 12:50:54 -0500

    added the logic behind generating the password, also fixed a bug where 'passwordLength' wasn't actually converting into a number before

--- script.js
+++ script.js
@@ -81,10 +81,7 @@ function generatePassword(){
   "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
   "T", "U", "V", "W", "X", "Y", "Z"]; 
-  var numberArray = Array(129); 
-  for (i = 0; i < 129; i++){
-    numberArray[i] = i; 
-  }
+  var numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   var specialCharacterArray = ["!","@","#", "$", "%","^", "&", "*", "(", ")"];
 
   console.log(numberArray); 
@@ -99,14 +96,22 @@ function generatePassword(){
 
   var passwordLength = prompt("How many characters do you want your password to be?", "8-128 characters. Please enter only numbers.");
   console.log(passwordLength);
-  Number(passwordLength);
+  passwordLength = Number(passwordLength);
+  
+  console.log(typeof passwordLength);
+  console.log(passwordLength);
+  
+  //resource I used to check if passwordLength was NaN: https://www.tutorialspoint.com/How-to-check-if-a-variable-is-NaN-in-JavaScript#:~:text=Use%20the%20Number.&text=The%20isNaN()%20method%20checks,always%20returns%20a%20false%20value.
+  if (isNaN(passwordLength)){
+    return "Please enter only numbers!";
+  }
 
   if (passwordLength < 8){
-    return "Password is too short. Please reload and try again."
+    return "Password is too short. Please try again."
   }
 
   if (passwordLength > 128){
-    return "Password is too long. Please reload and try again."
+    return "Password is too long. Please try again."
   }
 
   //info about using confirm() from: https://sabe.io/blog/javascript-yes-no-confirmation-box#:~:text=The%20best%20way%20to%20create,Ok%20and%20a%20Cancel%20button.
@@ -120,7 +125,57 @@ function generatePassword(){
   specialCharacterChosen = confirm("Do you want special characters in your password? Press OK for 'Yes' and CANCEL for 'No'");
   console.log("special characters: ", specialCharacterChosen);
 
+  if (!lowercaseChosen && !uppercaseChosen && !numberChosen && !specialCharacterChosen){
+    return "Please try again. At least one condition must be set!";
+  }
 
+  //randomNumber code source: https://www.w3schools.com/js/js_random.asp
+  var alphabetRandom = Math.floor(Math.random() * 26);
+  var alphabetRandom2 = Math.floor(Math.random() * 26);
+  var alphabetRandom3 = Math.floor(Math.random() * 26);
+  var alphabetRandom4 = Math.floor(Math.random() * 26);
+  var alphabetRandom5 = Math.floor(Math.random() * 26);
+  var numberRandom = Math.floor(Math.random() * 10);
+  var specialCharacterRandom = Math.floor(Math.random() * 10);
+
+  var passwordArray = Array(passwordLength); 
+  for (j = 0; j < passwordLength; j++){
+    if (lowercaseChosen){
+      passwordArray[j] = lowercaseArray[Math.floor(Math.random() *26)];
+    }
+    if (uppercaseChosen){
+      passwordArray[2] = uppercaseArray[Math.floor(Math.random() *26)];
+    }
+
+    if (numberChosen){
+      passwordArray[4] = numberArray[Math.floor(Math.random() *10)];
+    }
+
+    if (specialCharacterChosen){
+      passwordArray[6] = specialCharacterArray[Math.floor(Math.random() *10)];
+    }
+  }
+  console.log(passwordArray);
+
+  //shuffle the array 
+  function shuffleArray(array){
+    var currentIndex = array.length, randomIndex; 
+
+    while (currentIndex != 0){
+      randomIndex = Math.floor(Math.random() * currentIndex); 
+      currentIndex--; 
+
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+  }
+
+  shuffleArray(passwordArray);
+  console.log("shuffled password: " +passwordArray); 
+
+  var finalPassword = passwordArray.join("");
+  console.log(finalPassword);
+
+  return finalPassword; 
 
 
 
